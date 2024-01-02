977702acc5940f6882004730451f35ca6c30d7b2
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-02 18:53:49 +0100

    Add project files.

---
+++ TheSuiteSpot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheSuiteSpot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
